7c5728e0b610b88b1c33dfc437a4076acb60fa31
Wilker Machado <wilkerjmachado@gmail.com>
2021-05-19 17:48:05 -0300

    Estrutura inicial do backend

---
+++ backend-node/src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.status(200).send({
+        title: "Node API",
+        version: "1.0"
+    });
+});
+
+module.exports = router;
