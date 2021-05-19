94d20450eaa5e02beaf16bdcbcbf6c3728f9a3aa
Wilker Machado <wilkerjmachado@gmail.com>
2021-05-19 18:06:39 -0300

    Criação de rotas para funcionalidade de clientes e produtos

--- backend-node/src/app.js
+++ backend-node/src/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 const index = require('./routes/index');
+const clienteRoute = require('./routes/cliente');
+const produtoRoute = require('./routes/produto');
 
 app.use('/', index);
+app.use('/cliente', clienteRoute);
+app.use('/produto', produtoRoute);
 
 module.exports = app;
