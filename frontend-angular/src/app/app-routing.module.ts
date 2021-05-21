8126b034fa8171b3f0afd4ab729686d4c428b725
Wilker Machado <wilkerjmachado@gmail.com>
2021-05-21 09:02:57 -0300

    Ajuste na rota incial

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {CLIENTE_ROUTE} from "./view/cliente/cliente.route";
 import {PRODUTO_ROUTE} from "./view/produto/produto.route";
 import {PaginaNaoEncontradaComponent} from "./view/pagina-nao-encontrada/pagina-nao-encontrada.component";
+import {ClienteListComponent} from "./view/cliente/cliente.list.component";
 
 const routes: Routes = [
+
+  { path: '', component: ClienteListComponent },
 
   ...CLIENTE_ROUTE,
 
