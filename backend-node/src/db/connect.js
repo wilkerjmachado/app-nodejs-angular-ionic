c26ef4340ae4566c31991067d11ec8e39493727f
Wilker Machado <wilkerjmachado@gmail.com>
2021-05-21 08:57:28 -0300

    Configuração docker e docker compose

--- backend-node/src/db/connect.js
+++ backend-node/src/db/connect.js
@@ -1,2 +1,12 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/app');
+
+var databaseUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/app';
+
+mongoose.connect(databaseUrl);
+
+mongoose.connection.on('connected', function () {
+     console.log('mongoose connected to : ', databaseUrl);
+});
+mongoose.connection.on('error', function (err) {
+     console.log('mongoose connection error to : ', err);
+});
